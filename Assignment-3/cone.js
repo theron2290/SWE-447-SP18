7335b718c32ec2a87ac90aaf5859a2a16d9b0b12
Andrew Mack <norcalgiantsfan1@live.com>
2018-01-31 17:33:19 -0800

    Add files via upload

---
+++ Assignment-3/cone.js
@@ -0,0 +1,31 @@
+
+var gl = null;
+var cone = null; 
+
+function init() {
+    var canvas = document.getElementById( "webgl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    cone = new Cone(gl, 90);
+
+
+
+    if ( !gl ) {
+        alert("Unable to setup WebGL");
+        return;
+    }
+
+    gl.clearColor( 0.0,  0.0,  0.0,  1.0,);
+ 
+    render();
+    
+}
+
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+   
+	cone.render();
+}
+
+window.onload = init;
