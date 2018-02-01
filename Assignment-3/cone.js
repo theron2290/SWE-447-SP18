de0ca84187e82fb5eb117eea0b88638a45e329dc
Andrew Mack <norcalgiantsfan1@live.com>
2018-01-31 17:35:18 -0800

    Update cone.js

--- Assignment-3/cone.js
+++ Assignment-3/cone.js
@@ -8,13 +8,6 @@ function init() {
     gl = WebGLUtils.setupWebGL( canvas );
     cone = new Cone(gl, 90);
 
-
-
-    if ( !gl ) {
-        alert("Unable to setup WebGL");
-        return;
-    }
-
     gl.clearColor( 0.0,  0.0,  0.0,  1.0,);
  
     render();
